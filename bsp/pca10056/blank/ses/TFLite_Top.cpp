d28ca932e56b92e507eb2e0aca9a5533ef935fce
jimware <james.atwell@atwellengineering.com>
2023-02-21 19:55:18 -0700

    Added a few more pieces. Compiles and runs.

--- bsp/pca10056/blank/ses/TFLite_Top.cpp
+++ bsp/pca10056/blank/ses/TFLite_Top.cpp
@@ -1,4 +1,28 @@
 #include "tensorflow/lite/micro/micro_interpreter.h"
-
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/micro/micro_log.h"
+#include "TFLite_Top.h"
+#include "sine_wave_model_data.h"
 
 const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+int inference_count = 0;
+
+
+void setup_tf_system(void)
+{
+   tflite::InitializeTarget(); 
+
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  model = tflite::GetModel(g_sine_wave_data);
+
+  if (model->version() != TFLITE_SCHEMA_VERSION) 
+  {
+    MicroPrintf("Model provided is schema version %d not equal to supported version %d.",
+        model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+}
---
+++ bsp/pca10056/blank/ses/TFLite_Top.h
@@ -0,0 +1,6 @@
+#ifndef TFLITE_TOP_H_
+#define TFLITE_TOP_H_
+
+void setup_tf_system(void);
+
+#endif
